751f09892e45836f5bd7b93ac83645de89569984
xqwtxon <136815140144@ncr2.deped.gov.ph>
2023-11-25 11:04:47 +0800

    Remove useless..

--- src/Zuri.php
+++ src/Zuri.php
@@ -52,36 +52,6 @@ use function in_array;
 use function is_string;
 use function microtime;
 
-/**
- * **Zuri Anticheat**
- *
- * `Anticheat Development` - *Fast-development mode*
- *
- * TODO:
- * - Improve KillAura
- * - Improve more checks on Reach
- * - Velocity check
- * - Bad packets check
- * - Chest stealer check
- * - Edition Faker (for most advance clients)
- * - Improve player speed check
- *
- * Checks:
- * - Reach (90% done, some false-positive)
- * - Timer (100% done, unchecked)
- * - Bad Packets (100% done, unchecked)
- * - Instabreak or Nuke (100% done, checked & clean)
- * - Flight (50% done, unchecked)
- * - Glitch (100% done, unchecked)
- * - NoClip (100% done, checked & clean)
- * - Killaura (20% done, unchecked)
- * - NoPacket/Blink (100% done, unchecked)
- * - Autoclicker (100% done, unchecked)
- * - Chest stealer (0% done)
- * - Edition Faker (100% done, unchecked)
- * - Speed (90% done)
- * - Velocity (0% done)
- */
 abstract class Zuri {
 	public const PREFIX = TF::GRAY . "(" . TF::YELLOW . TF::BOLD . "Zuri" . TF::RESET . TF::GRAY . ")";
 	public const ARROW = TF::RESET . TF::AQUA . TF::BOLD . "»" . TF::RESET;
